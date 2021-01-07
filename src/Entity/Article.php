e7cf3cc7d7b561daf561fafec3df1e6ff095e58a
hamza-novatech <galant.vert@gmail.com>
2021-01-07 21:07:16 +0100

    init

---
+++ src/Entity/Article.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ArticleRepository;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ * @Table(name="articles")
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $slug;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private \DateTimeImmutable $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private \DateTimeImmutable $publishedAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private \DateTime $editeAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeImmutable $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getEditeAt(): ?\DateTimeInterface
+    {
+        return $this->editeAt;
+    }
+
+    public function setEditeAt(?\DateTimeInterface $editeAt): self
+    {
+        $this->editeAt = $editeAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
